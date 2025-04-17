6e5aedb4a4757b25997f75406c175d18e9f2bff4
AkiMathur <90270340+AkiMathur@users.noreply.github.com>
2025-04-17 22:02:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (93.83%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,7 +1,6 @@
 class Solution:
     def merge(self, nums1, m, nums2, n):
         for i in range(n):
-            nums1[m] = nums2[i]
-            m+=1
+            nums1[m+i] = nums2[i]
         nums1.sort()
         #done
