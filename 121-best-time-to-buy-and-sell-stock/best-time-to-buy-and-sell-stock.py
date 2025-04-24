0a1bab8659913be2a6712af9bdd2846e6be48232
AkiMathur <90270340+AkiMathur@users.noreply.github.com>
2025-04-24 18:30:30 +0530

    Time: 23 ms (94.37%) | Memory: 26.8 MB (78.03%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -6,6 +6,7 @@ class Solution:
         for i in range(1,len(prices)):
             if prices[i] < buying:
                 buying = prices[i]
+                continue
             if (prices[i] - buying) > profit:
                 profit = prices[i] - buying
         return profit
