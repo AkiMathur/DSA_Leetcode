b4a8ca0e568c083c5dc011cdc0396cc769b67fc3
AkiMathur <90270340+AkiMathur@users.noreply.github.com>
2025-05-05 23:57:57 +0530

    Time: 15 ms (96.93%) | Memory: 23.2 MB (93.52%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,13 +3,15 @@ class Solution:
         n = len(nums)
         answer = [1] * n
 
-        # First pass: prefix product
-        for i in range(1, n):
-            answer[i] = answer[i - 1] * nums[i - 1]
+        # Pass 1: prefix products
+        prefix = 1
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
 
-        # Second pass: suffix product
+        # Pass 2: suffix products
         suffix = 1
-        for i in range(n - 1, -1, -1):
+        for i in range(n-1, -1, -1):
             answer[i] *= suffix
             suffix *= nums[i]
 
