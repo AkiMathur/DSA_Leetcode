0dc98c649ff11f6f6e10fe802c6be891b3dd6353
AkiMathur <90270340+AkiMathur@users.noreply.github.com>
2025-10-31 11:32:23 +0530

    Time: 12 ms (83.24%) | Memory: 18.2 MB (13.28%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,11 +1,16 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        cons = dict()
+        mag_l = dict()
+        ran_l = dict()
 
         for letters in magazine:
-            cons[letters] = magazine.count(letters)
+            if letters not in mag_l:
+                mag_l[letters] = magazine.count(letters)
         for letters in ransomNote:
-            if letters in cons and ransomNote.count(letters) <= cons[letters]:
+            if letters not in ran_l:
+                ran_l[letters] = ransomNote.count(letters)
+        for i in ran_l.keys():
+            if i in mag_l and ran_l[i]<=mag_l[i]:
                 continue
             else:
                 return False
